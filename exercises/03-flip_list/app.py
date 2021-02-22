bd8303ec646fe3f733222d9c25428909c68f7501
Daniel Ipince <ipince22@gmail.com>
2021-02-22 21:36:00 +0000

    samples

--- exercises/03-flip_list/app.py
+++ exercises/03-flip_list/app.py
@@ -1,3 +1,6 @@
 arr = [45, 67, 87, 23, 5,  32, 60]
-
+newarr=[]
 #your code below:
+for i in reversed(arr):
+    newarr.append(i)
+print(newarr)
--- exercises/04.1-count_on/app.py
+++ exercises/04.1-count_on/app.py
@@ -1,7 +1,13 @@
 
 my_list = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
-
+hello=[]
 
 #your code go here:
+for x in range(len(my_list)):
+    if isinstance(my_list[x],dict):
+        hello.append(my_list[x])
+    elif isinstance(my_list[x],list):
+        hello.append(my_list[x])
+
 
 print(hello)
