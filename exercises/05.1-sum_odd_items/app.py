89187ef97c9641218fd4d016af6629d1582523d4
Daniel Ipince <ipince22@gmail.com>
2021-02-22 23:05:47 +0000

    samples

--- exercises/05.1-sum_odd_items/app.py
+++ exercises/05.1-sum_odd_items/app.py
@@ -1,5 +1,11 @@
 arr = [4,5,734,43,45,100,4,56,23,67,23,58,45]
-
 #Your code go here:
+  
+def sumOdds(array):
+  sumat=0
+  for i in range(len(array)):
+    if array[i]%2!=0:
+      sumat = sumat + array[i]
+  return sumat
 
-
+print(sumOdds(arr))
